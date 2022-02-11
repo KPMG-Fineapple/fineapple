ca15e6e307a7ec7c00f98bb9b9ab2adab0712665
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-11 21:22:23 +0900

    [BE] : 파이썬 데이터 값 전달 로직 수정
    * 파이썬 파일로부터 결과값을 안받아오는 이슈가 있어서 처리
    * FE 테스트를 위해 FE 브랜치에 둠

--- BE/src/consumption/consumption.service.ts
+++ BE/src/consumption/consumption.service.ts
@@ -1,21 +1,15 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { spawn } from 'child_process';
+import { spawnSync } from 'child_process';
 
 @Injectable()
 export class ConsumptionService {
   private logger = new Logger('ConsumptionService');
 
-  getConsumption(address: string) {
-    const process = spawn('python3', ['../AI/consumption-model.py']);
-
-    process.stdout.on('data', function (data) {
-      const result = JSON.parse(data.toString());
-      Logger.debug('GET DATA :' + data.toString());
-      return result;
-    });
-
-    process.stderr.on('data', function (data) {
-      Logger.error(data.toString());
-    });
+  async getConsumption(address: string) {
+    const process = spawnSync('python3', ['../AI/consumption-model.py']);
+    const result = JSON.parse(
+      Buffer.from(process.stdout.toJSON().data).toString(),
+    );
+    return result;
   }
 }
