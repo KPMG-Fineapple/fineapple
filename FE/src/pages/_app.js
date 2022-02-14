54c20c322a3cce05a46c463f437377f135cb2df6
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-14 22:10:44 +0900

    feature: 페이지 타이틀 변경

--- FE/src/pages/_app.js
+++ FE/src/pages/_app.js
@@ -18,7 +18,7 @@ const App = (props) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>Material Kit Pro</title>
+        <title>Fine apple</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <LocalizationProvider dateAdapter={AdapterDateFns}>
