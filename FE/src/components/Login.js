783d77874a3989fce146f119bae78901f66f9829
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-14 22:35:24 +0900

    refactor : 로그인 화면 수정

--- FE/src/components/Login.js
+++ FE/src/components/Login.js
@@ -6,8 +6,10 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import Image from "next/image";
 
 export default function Login({ updateLogin }) {
   const router = useRouter();
@@ -30,95 +32,111 @@ export default function Login({ updateLogin }) {
   };
 
   return (
-    <Container component="main" maxWidth="xs" style={{ marginTop: "20vh" }}>
+    <>
       {!signUp ? (
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            로그인
-          </Typography>
+        <Container component="main" maxWidth="xs" style={{ marginTop: "20vh" }}>
           <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="아이디"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="비밀번호"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => router.push("/dashboard")}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              로그인
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              noValidate
+              sx={{ mt: 1 }}
             >
-              로그인하기
-            </Button>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => router.push("/dashboard")}
-            >
-              처음 오셨어요? 우리집 예상 수익률 계산하기
-            </Button>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="아이디"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="비밀번호"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => router.push("/dashboard")}
+              >
+                로그인하기
+              </Button>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => setSignUp(true)}
+              >
+                처음 오셨어요? 우리집 예상 수익률 계산하기
+              </Button>
+            </Box>
           </Box>
-        </Box>
+        </Container>
       ) : (
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          noValidate
-          sx={{ mt: 30 }}
+        <Grid
+          container
+          justify="center"
+          alignItems="center"
+          direction="column"
+          spacing={5}
+          style={{ marginTop: "25vh" }}
         >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="address"
-            label="거주하시는 주소를 입력해주세요"
-            onChange={(e) => setAddress(e.target.value)}
-            id="address"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            onClick={handleLogin}
-          >
-            예측 결과 보러 가기
-          </Button>
-        </Box>
+          <Grid item>
+            <Image
+              src="/static/logo2.svg"
+              alt="logo"
+              width="192"
+              height="192"
+            />
+          </Grid>
+          <Grid item>
+            <Box component="form" onSubmit={handleSubmit} noValidate>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="address"
+                label="거주하시는 주소를 입력해주세요"
+                onChange={(e) => setAddress(e.target.value)}
+                id="address"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => router.push("/dashboard")}
+              >
+                예측 결과 보러 가기
+              </Button>
+            </Box>
+          </Grid>
+        </Grid>
       )}
-    </Container>
+    </>
   );
 }
