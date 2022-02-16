22acefd2b928bc4f7b6b86be2dbc48b4ad87d94b
NEM-NE <dlatqdlatq@mju.ac.kr>
2022-02-16 23:02:20 +0900

    feature: 투자 정보를 보여 줄 수 있는 아코디언 컴포넌트 추가

---
+++ FE/src/components/invest/accordion.js
@@ -0,0 +1,38 @@
+import {
+  Typography,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export function InvestAccordions(props) {
+  const { title, items } = props;
+  return (
+    <>
+      <Typography align="left" variant="subtitle1" sx={{ mb: "20px" }}>
+        {title}
+      </Typography>
+      {items.map((item) => {
+        return (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>{item}</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+                eget.
+              </Typography>
+            </AccordionDetails>
+          </Accordion>
+        );
+      })}
+    </>
+  );
+}
