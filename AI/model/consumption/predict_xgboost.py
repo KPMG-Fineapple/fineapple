8d899ba25464af2172d37be45a497c49192a23d7
4923 <www_1216@hufs.ac.kr>
2022-02-21 15:04:14 +0900

    Fix Consumption 예측 모델 전달 이슈
    - issue #36
    - 문제가 완전히 해결되지는 않았으므로 open 유지

--- AI/model/consumption/predict_xgboost.py
+++ AI/model/consumption/predict_xgboost.py
@@ -34,41 +34,36 @@ def train_xgboost(dataset: pd.DataFrame, x_test):
     x_train, x_valid, y_train, y_valid = split_dataset(
         dataset, testset_size=0.2, do_shuffle=False)
 
-    # params = {'booster':  ['gbtree', 'gblinear', 'dart'],
-    #           'max_depth': [i for i in range(3, 20, 2)],
-    #           'learning_rate': [0.01, 0.05, 0.1],
-    #           'n_estimators': [100, 500, 1000],
-    #           'colsample_bytree': [0.3, 0.7],
-    #           'n_estimators': [i for i in range(100, 3000, 500)]}
+    # params = {'colsample_bytree': 0.3, 'learning_rate': 0.05,
+    #           'max_depth': 3, 'n_estimators': 100}
 
-    params = {'colsample_bytree': 0.3, 'learning_rate': 0.05,
-              'max_depth': 3, 'n_estimators': 100}
     # Fitting 5 folds for each of 54 candidates, totalling 270 fits
     # Best parameters: {'colsample_bytree': 0.3, 'learning_rate': 0.05, 'max_depth': 3, 'n_estimators': 100}
     # Lowest RMSE:  7.0607787236388955
 
     # model
-    xgb_reg = XGBRegressor(seed=0)
-
-    # Grid Search CV
-    clf = GridSearchCV(estimator=xgb_reg,
-                       param_grid=params,
-                       scoring='neg_mean_squared_error',
-                       verbose=1)
-    clf.fit(x_train, y_train)
-    print("Best parameters:", clf.best_params_)
-    print("Lowest RMSE: ", (-clf.best_score_)**(1/2.0))
-
-    # train
-    xgb_reg.fit(
-        x_train,
-        y_train,
+    xgb_reg = XGBRegressor(
         colsample_bytree=0.3,
         learning_rate=0.05,
         max_depth=3,
         n_estimators=100,
         eval_set=[(x_train, y_train), (x_valid, y_valid)],
         verbose=False,
+        seed=0)
+
+    # Grid Search CV
+    # clf = GridSearchCV(estimator=xgb_reg,
+    #                    param_grid=params,
+    #                    scoring='neg_mean_squared_error',
+    #                    verbose=1)
+    # clf.fit(x_train, y_train)
+    # print("Best parameters:", clf.best_params_)
+    # print("Lowest RMSE: ", (-clf.best_score_)**(1/2.0))
+
+    # train
+    xgb_reg.fit(
+        x_train,
+        y_train,
     )
 
     x_test.drop("일시", axis=1, inplace=True)   # 학습에서 '일시' 사용하지 않으므로 drop
@@ -90,7 +85,7 @@ def visual_xgboost(y_valid, pred, xgbr) -> None:
     # rankdir : 트리의 방향, 디폴트는 위아래 방향
     # rankdir="LR" : 왼쪽에서 오른쪽 방향으로 트리를 보여준다.
     xgb.plot_importance(xgbr)
-    xgb.plot_tree(xgbr, num_trees=0, rankdir='LR')
+    xgb.plot_tree(xgbr, num_trees=0, rankdir='TB')
 
     fig = plt.gcf()
     fig.set_size_inches(150, 100)
